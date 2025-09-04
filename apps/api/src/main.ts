36faef7c12390bfd3aac00289c7892eaac5eba5d
(주)피드백1 <feedback1@jupideubaeg1ui-iMac.local>
2025-09-04 17:06:05 +0900

    배포 테스트

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: configService.get('FRONTEND_URL', 'http://localhost:3000'),
+    origin: configService.get('FRONTEND_URL', 'http://localhost:3000', 'https://feedbackmall.com'),
     credentials: true,
   });
 

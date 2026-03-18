8edb4c6cf8dd15fa11820600fe3ac982691f6df4
(주)피드백1 <feedback1@jupideubaeg1ui-iMac.local>
2026-03-18 11:03:30 +0900

    카테고리 수정

--- apps/web/src/components/layout/Header.tsx
+++ apps/web/src/components/layout/Header.tsx
@@ -201,7 +201,7 @@ const Header = () => {
               href="/category/top10" 
               className="text-gray-600 hover:text-gray-900 transition-colors duration-200 font-medium text-sm sm:text-base whitespace-nowrap flex items-center"
             >
-              <span className="text-orange-500 mr-1">★</span>
+              <span className="text-orange-500 mr-1"></span>
               3월달 한정수량
             </Link>
             <Link 
